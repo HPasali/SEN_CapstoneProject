f8eae02ca127a04a436ce9c3d2fcdff8cd042379
Onur Ozkinaci <onurozkinaci@gmail.com>
2023-05-04 18:13:31 +0300

    email regex pattern is updated.

--- app/src/main/java/helpers/RegexPatterns.java
+++ app/src/main/java/helpers/RegexPatterns.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 //=>This class will be used as a helper class in order to control the entered inputs from the user by calling it on the Login, Register, EditProfileFragment, etc. pages;
 public class RegexPatterns {
     //---------Regex Patterns;------------------------
-    public static final Pattern EMAIL_PATTERN = Pattern.compile("[a-z0-9]+@[a-z]+\\.[a-z]{2,3}"); //username@domain.com
+    public static final Pattern EMAIL_PATTERN = Pattern.compile("[a-z0-9.-]+@[a-z]+\\.[a-z]{2,3}"); //username@domain.com
     public static final Pattern PASSWORD_PATTERN = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$%^&+=_-])(?=\\S+$).{6,16}$"); //min 6 characters are needed to be saved on Firebase Db.
     //'P@ssw1rd' =>Contains at least one uppercase letter (P), one lowercase letter (s), one digit (0), and one special character (@), Does not contain any whitespace characters and meets the length requirement
     // of 6 characters.
